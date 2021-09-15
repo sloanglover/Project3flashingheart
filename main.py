253623880eafb27446fbf8bd898ca0b10148157e
sloanglover <89929128+sloanglover@users.noreply.github.com>
2021-09-15 12:15:39 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+    basic.clear_screen()
+    basic.pause(500)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.clear_screen()
+    basic.pause(500)
+basic.forever(on_forever)
